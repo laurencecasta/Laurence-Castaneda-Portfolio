42b14a637323a1d1bdb6d97442d200cd8bd252ac
Laurence Castaneda <laurence.d.castaneda@gmail.com>
2020-11-11 13:41:05 -0600

    add scroll indicator and style

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,20 +3,44 @@ import React from 'react'
 import './styles.css'
 
 import heroImg from './portfolio-hero.jpg'
+import downArrow from './downArrow.png'
 
 function Hero() {
+  const heroImgStyle = {
+    width: '428px',
+    height: '497px',
+    'border-radius': '20px',
+    'margin-top': '-441px',
+    position: 'relative',
+    right: '92px',
+  }
+
+  const downArrowStyle = {
+    width: '25.56px',
+    height: '15.56px',
+    right: '0px',
+    display: 'block',
+    margin: 'auto',
+    'margin-top': '25px',
+  }
+  
   return (
-    <section>
-      <div className='text'>
-        <h1>Hi, my name is Laurence.</h1>
-        <h1 className='who-i-am'>I'm a full-stack developer.</h1>
-        <p>I help businesses expand their vision by leveraging the power of tech to solve important problems.</p>
+    <div>
+      <section>
+        <div className='text'>
+          <h1>Hi, my name is Laurence.</h1>
+          <h1 className='who-i-am'>I'm a full-stack developer.</h1>
+          <p>I help businesses expand their vision by leveraging the power of tech to solve important problems.</p>
+        </div>
+        <div className='image'>
+          <div className='rectangle'></div>
+          <img style={ heroImgStyle } src={heroImg} alt='Hero Portrait'></img>
+        </div>
+      </section>
+      <div className='down-arrow-container'>
+        <img style={downArrowStyle} src={downArrow} alt='arrow pointing down'></img>
       </div>
-      <div className='image'>
-        <div className='rectangle'></div>
-        <img src={heroImg} alt='Hero Portrait'></img>
-      </div>
-    </section>
+    </div>
   )
 }
 
