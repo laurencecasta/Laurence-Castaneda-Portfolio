01a71b80d70a5892e45df782a89f1b46075442de
Laurence Castaneda <laurence.d.castaneda@gmail.com>
2020-11-18 12:16:40 -0600

    complete about section image and title

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,29 @@
 import React from 'react'
 
+import './styles.css'
+
+import aboutImg from './about-picture.jpg'
+import underscore from './underscore.png'
+
 function About() {
+  const underscoreStyle = {
+    width: '30px',
+    height: '7px',
+    'padding-top': '47px',
+  }
   return (
-    <div>About goes here</div>
+    <section className='aboutSection'>
+      <div className='aboutImg'>
+        <div className='aboutRectangle'></div>
+        <img src={aboutImg} alt='about'></img>
+      </div>
+      <div className='text'>
+        <div className='title'>
+          <h2>About Me</h2>
+          <img style={underscoreStyle} src={underscore} alt='underscore'></img>
+        </div>
+      </div>
+    </section>
   )
 }
 
