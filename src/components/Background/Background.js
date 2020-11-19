c20ce85f0c8d44e9955d7b1d5bd0116140c1cf10
Laurence Castaneda <laurence.d.castaneda@gmail.com>
2020-11-19 15:39:06 -0600

    add next background rectangle

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -8,6 +8,7 @@ function Background() {
       <div className='rectangle-1'></div>
       <div className='rectangle-2'></div>
       <div className='rectangle-3'></div>
+      <div className='rectangle-4'></div>
     </div>
   )
 }
