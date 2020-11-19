ad92e12bd2056b844bfae43929ecbfc3c6aed8f5
Laurence Castaneda <laurence.d.castaneda@gmail.com>
2020-11-19 13:36:15 -0600

    create quote component and style with props

--- src/components/Testimonies/Testimonies.js
+++ src/components/Testimonies/Testimonies.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
+import Quote from '../Quote/Quote'
+
 import './styles.css'
 
 import underscore from './underscore.png'
+import trey from './PT.jpg'
 
 function Testimonies() {
   const underscoreStyle = {
@@ -15,6 +18,20 @@ function Testimonies() {
       <div className='title'>
         <h2>Testimonials</h2>
         <img style={underscoreStyle} src={underscore} alt='underscore'></img>
+      </div>
+      <div className='quotes'>
+        <Quote
+          quote='You can turn an idea into reality with very little time and very few resources. The only potential bottleneck is your own creativity and your own mind, so the possibilities are endless.'
+          avatar={trey}
+          name='Trey Rose'
+          occupation='lead pastor'
+        />
+        <Quote
+          quote='You can turn an idea into reality with very little time and very few resources. The only potential bottleneck is your own creativity and your own mind, so the possibilities are endless.'
+          avatar={trey}
+          name='Trey Rose'
+          occupation='lead pastor'
+        />
       </div>
     </section>
   )
