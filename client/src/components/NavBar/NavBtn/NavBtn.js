856540c37977876e84dc33d2714da13a6d1aca64
Laurence Castaneda <laurence.d.castaneda@gmail.com>
2020-12-12 22:29:06 -0600

    style animation for navigation hamburger

--- client/src/components/NavBar/NavBtn/NavBtn.js
+++ client/src/components/NavBar/NavBtn/NavBtn.js
@@ -2,17 +2,8 @@ import React from 'react'
 
 function NavBtn(props) {
   return (
-    props.display ?
-    <div className='toggle-button'>
-      <span style={{fontSize: '25px', lineHeight: '25px', color: 'white', marginTop: '-5px'}}>
-        <i className="fa fa-times"></i>
-      </span>
-    </div>
-    :
-    <div className='toggle-button'>
-      <span className='bar'></span>
-      <span className='bar'></span>
-      <span className='bar'></span>
+    <div className={props.display ? 'toggle-button open' : 'toggle-button'}>
+      <div className='toggle-button-burger'></div>
     </div>
   )
 }
