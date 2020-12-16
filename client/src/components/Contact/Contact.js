a390041cc7413c36a7bf796c59559a9eddd8fbf3
Laurence Castaneda <laurence.d.castaneda@gmail.com>
2020-12-16 09:24:06 -0600

    validate contact form on frontend

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -14,11 +14,16 @@ const initialState = {
   name: '',
   email: '',
   message: '',
+  error: {
+    nameErr: '',
+    emailErr: '',
+    messageErr: '',
+  },
 };
 
 const ACTIONS = {
   CONTROL_FORM: 'control-form',
-  RESET_FORM: 'reset-form',
+  HANDLE_ERROR: 'handle-error',
 }
 
 function reducer(state, action) {
@@ -28,12 +33,14 @@ function reducer(state, action) {
         ...state,
         [action.payload.field]: action.payload.value,
       };
-    case ACTIONS.RESET_FORM:
+    case ACTIONS.HANDLE_ERROR:
       return {
-        name: '',
-        email: '',
-        message: '',
-      };
+        ...state,
+        error: {
+          ...state.error,
+          [action.payload.errorName]: action.payload.value,
+        }
+      }
     default:
       return state;
   }
@@ -47,10 +54,75 @@ function Contact() {
     dispatch({type: ACTIONS.CONTROL_FORM, payload: { field: e.target.name, value: e.target.value }})
   }
 
-  const { name, email, message } = state;
+  const { name, email, message, error} = state;
+
+  const validate = () => {
+    let nameErr = '', emailErr = '', messageErr = '';
+    if (!email) {
+      emailErr = 'Please enter your email address';
+      dispatch({
+        type: ACTIONS.HANDLE_ERROR,
+        payload: {errorName: 'emailErr', value: emailErr
+      }});
+    } else if (!email.match(/^([a-zA-Z0-9]+)@([a-zA-Z]+).([a-zA-Z]+)$/)) {
+      emailErr = 'Please enter your email address in the format: name@example.com';
+      dispatch({
+        type: ACTIONS.HANDLE_ERROR,
+        payload: {errorName: 'emailErr', value: emailErr
+      }});
+    } else {
+      dispatch({
+        type: ACTIONS.HANDLE_ERROR,
+        payload: {errorName: 'emailErr', value: ''
+      }});
+    }
+
+    if (!name) {
+      nameErr = 'Please enter your name';
+      dispatch({
+        type: ACTIONS.HANDLE_ERROR,
+        payload: {errorName: 'nameErr', value: nameErr
+      }});
+    } else if (name.length <= 3) {
+      nameErr = 'Please enter more than 3 characters';
+      dispatch({
+        type: ACTIONS.HANDLE_ERROR,
+        payload: {errorName: 'nameErr', value: nameErr}
+      });
+    } else {
+      dispatch({
+        type: ACTIONS.HANDLE_ERROR,
+        payload: {errorName: 'nameErr', value: ''
+      }});
+    }
+
+    if (!message) {
+      messageErr = 'Please enter a message';
+      dispatch({
+        type: ACTIONS.HANDLE_ERROR,
+        payload: {errorName: 'messageErr', value: 'Please enter a message'
+      }});
+    } else {
+      dispatch({
+        type: ACTIONS.HANDLE_ERROR,
+        payload: {errorName: 'messageErr', value: ''
+      }});
+    }
+
+    if (emailErr || nameErr || messageErr) {
+      return false;
+    }
+    return true;
+  }
 
   const onSubmit = (e) => {
     e.preventDefault();
+
+    let isValid = validate();
+    if (!isValid) {
+      console.log('invalid email!');
+      return;
+    }
 
     const postEmailData = async () => {
       try {
@@ -84,7 +156,7 @@ function Contact() {
           <img className='check' src={check} alt='check'></img>
           <h4 className='successTitle'>Your message was sent!</h4>
           <p className='successMessage'>
-            Thanks for connecting. I’ll be in touch within the next 24 hours! In the meantime, you can download my resume or see more work.
+            Thanks for connecting. I’ll be in touch within the next 24 hours! In the meantime, you can download my resume or view my work.
           </p>
           <div className='project-links success-link'>
             <a className='live-link' href='/Laurence-Castaneda-Resume-2020.docx' download>Download Resume</a>
@@ -93,11 +165,14 @@ function Contact() {
         </div> :
         <form id='contact-form' onSubmit={onSubmit}>
           <label htmlFor='name-input'>Name</label>
-          <input type='text' id='name-input' name='name' onChange={onChange} value={name}/>
+          <input type='text' id='name-input' name='name' onChange={onChange} onBlur={() => validate()} value={name}/>
+          <div className='error'>{error.nameErr}</div>
           <label htmlFor='email'>Email</label>
-          <input type='text' id='email' name='email' onChange={onChange} value={email}/>
+          <input type='text' id='email' name='email' onChange={onChange} onBlur={() => validate()} value={email}/>
+          <div className='error'>{error.emailErr}</div>
           <label htmlFor='message'>Message</label>
-          <textarea id='message' name='message' onChange={onChange} value={message}/>
+          <textarea id='message' name='message' onChange={onChange} onBlur={() => validate()} value={message}/>
+          <div className='error'>{error.messageErr}</div>
           <button>Submit</button>
         </form>}
       </div>
